c78fca131e2ca98eb202f4608157cc41b77868b7
DGordonEFS <dale.efs@gmail.com>
2019-12-23 11:42:17 -0500

    Home Screen Transitions

--- src/components/home/featuredpair.js
+++ src/components/home/featuredpair.js
@@ -3,11 +3,19 @@ import * as React from "react";
 export default class FeaturedPair extends React.Component {
   constructor(props) {
     super(props);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+  }
+
+  onMouseEnter() {
+    console.log("mouse enter");
   }
 
   render() {
     return (
-      <div className="HomeTitleFeaturedPair">
+      <div
+        className="HomeTitleFeaturedPair"
+        onMouseEnter={() => this.onMouseEnter()}
+      >
         <img
           className="HomeTitleFeaturedPairLogo"
           src="https://da-content.digital.scholastic.com/bfx/prod/assets/img/weston-woods-color.png"
